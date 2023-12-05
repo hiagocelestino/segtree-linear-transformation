499bf7fb8db70f80c86a4d07679cfdf4940a38c0
hiago <hiago.celestino@hotmail.com>
2023-12-05 04:59:46 -0300

    improves

--- include/node.hpp
+++ include/node.hpp
@@ -5,10 +5,14 @@
 class Node{
     public:
         Node();
-        Node(unsigned long long int a00, unsigned long long int a01, unsigned long long int a10, unsigned long long int a11);
+        Node(
+            unsigned long long int a00, unsigned long long int a01,
+            unsigned long long int a10, unsigned long long int a11
+        );
         ~Node();
         void print();
-       unsigned long long int (*getMatrix())[2];
+        unsigned long long int (*getMatrix())[2];
+
     private:
        unsigned long long int _matrix[2][2] = {0};
 
